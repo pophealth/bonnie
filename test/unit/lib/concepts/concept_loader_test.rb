ec8b237c53272de39a5a577107ea43333dad9bd1
Andy Gregorowicz <andy@mitre.org>
2012-09-18 14:55:17 -0400

    Adding testing to build_relationships. What gets tested depends on what Ruby implementation you use.

--- test/unit/lib/concepts/concept_loader_test.rb
+++ test/unit/lib/concepts/concept_loader_test.rb
@@ -18,4 +18,20 @@ class ConceptLoaderTest < ActiveSupport::TestCase
     concept = Concept.first
     assert concept.oids.include?("2.16.840.1.113883.3.464.0002.1138")
   end
+
+  test "build relationships" do
+    assert_equal 0, Concept.count
+    c1 = FactoryGirl.create(:concept)
+    c1.code_sets << FactoryGirl.build(:code_set)
+    c2 = FactoryGirl.create(:concept)
+    c2.code_sets << FactoryGirl.build(:code_set)
+
+    Concepts::ConceptLoader.build_relationships
+
+    c1.reload
+
+    assert_equal 1, c1.concept_relationships.size
+    cr = c1.concept_relationships.first
+    assert c2.id == cr.related_concept.id    
+  end
 end
