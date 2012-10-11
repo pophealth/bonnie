f80788d31d7ab913bfad3d322d581a17af476589
Marc Hadley <mhadley@mitre.org>
2012-10-11 11:56:22 -0400

    Add support for specifying race in patient editor

--- app/controllers/measures_controller.rb
+++ app/controllers/measures_controller.rb
@@ -389,6 +389,7 @@ class MeasuresController < ApplicationController
 
     ['first', 'last', 'gender', 'expired', 'birthdate', 'description', 'description_category'].each {|param| patient[param] = params[param]}
     patient['ethnicity'] = {'code' => params['ethnicity'], 'codeSystem' => 'CDC'}
+    patient['race'] = {'code' => params['race'], 'codeSystem' => 'CDC'}
     patient['source_data_criteria'] = JSON.parse(params['data_criteria'])
     patient['measure_period_start'] = params['measure_period_start'].to_i
     patient['measure_period_end'] = params['measure_period_end'].to_i
